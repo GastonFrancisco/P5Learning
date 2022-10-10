3c237789d7bd823f9baa427099f0e51ef68c428a
GastonFrancisco <fgariel97@gmail.com>
2022-10-10 17:12:59 -0300

    New commit

--- force-app/main/default/lwc/unitContent/unitContent.js
+++ force-app/main/default/lwc/unitContent/unitContent.js
@@ -1,6 +1,6 @@
 import { LightningElement, api, wire, track } from 'lwc';
-import getTrailWrapper from '@salesforce/apex/UnitService.getUnitWrapper';
 import getUnitWrapper from '@salesforce/apex/UnitService.getUnitWrapper';
+import metodo from '@salesforce/apex/UnitService.metodo';
 export default class UnitContent extends LightningElement {
     @api recordId
     error = undefined;
@@ -20,39 +20,15 @@ export default class UnitContent extends LightningElement {
         }
     }
 
-    // get correctUnit(){
-    //     return this.thisUnit.contains()
-    // }
-
-    // connectedCallback(){
-    //     getUnitWrapper({unitId: '$recordId'}).then((data)=>{
-    //         this.thisUnit = data.thisUnit;
-    //         this.thisQuestions = data.thisQuestions;
-    //     }).catch((error) =>{
-    //         this.error = error;
-    //     })
-    // }
-    // @wire(getUnitWrapper, {unitId: '$recordId'})
-    // unit({ data, error }) {
-    //     if(data) {
-    //         this.wrapper = data;
-    //         this.thisUnit = data.thisUnit;
-    //         this.thisQuestions = data.thisQuestions;
-    //     }else if(error) {
-    //         this.error = error;
-    //     }
-    // }
-    
-    // get unit(){
-    //     return this.thisUnit = this.wrapper.data.thisUnit;
-    // }
-
-    handleClick(){
-        console.log('WRAPPER');
-        console.log(this.wrapper)
-        console.log('UNIT');
-        console.log(this.thisUnit);
-        console.log('QUESTIONS');
-        console.log(this.thisQuestions);
+    renderedCallback(){
+        console.log(this.recordId);
+        metodo({unitId: this.recordId})
+        .then(
+            console.log('Todo joya')
+        )
+        .catch((error) =>
+            console.log(error.body.message)
+        )
     }
+
 }
